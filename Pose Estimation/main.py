2624f0e010a8074940bcdfc9778234d4a4228a24
Shantanu Roy <shan.roy1999@gmail.com>
2021-05-14 22:09:57 +0530

    Add files via upload

---
+++ Pose Estimation/main.py
@@ -0,0 +1,27 @@
+import cv2
+import time
+import poseModule as pm
+
+cap = cv2.VideoCapture('1.mp4')
+pTime = 0
+detector = pm.poseDetector()
+while True:
+    success, img = cap.read()
+    img = detector.findPose(img)
+    lmList = detector.findPosition(img, draw=False)
+    if len(lmList) != 0:
+        print(lmList[14])
+        cv2.circle(img, (lmList[14][1], lmList[14][2]), 10, (0, 0, 255), cv2.FILLED)
+
+    cTime = time.time()
+    fps = 1 / (cTime - pTime)
+    pTime = cTime
+
+    cv2.putText(img, str(int(fps)), (70, 50), cv2.FONT_HERSHEY_PLAIN, 3,
+                    (255, 0, 0), 3)
+
+    cv2.imshow("Image", img)
+    k = cv2.waitKey(1)
+    if k==27:
+        break
+cv2.destroyAllWindows()
