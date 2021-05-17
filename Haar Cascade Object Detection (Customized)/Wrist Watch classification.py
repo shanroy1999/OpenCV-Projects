1985780c2f7dc97f355a18ac389e862569ba1898
Shantanu Roy <shan.roy1999@gmail.com>
2021-05-17 11:27:21 +0530

    Update Wrist Watch classification.py

--- Haar Cascade Object Detection (Customized)/Wrist Watch classification.py
+++ Haar Cascade Object Detection (Customized)/Wrist Watch classification.py
@@ -1,5 +1,23 @@
 import cv2
 
+""" For creating the custom classifier :
+
+1) Download the software - "Cascade Trainger GUI (version 3.3.1)"
+2) Download / capture multiple images for the object which you want to detect / classify. (Example => Image of Wrist Watch)
+3) Download / capture other images of random things which are different from the object which you want to detect / classify (Example => Image of bottle / glass / car)
+4) Create a new folder "images" and create 2 subdirs "pos" and "neg" inside it
+6) Inside the "pos" folder copy the images of the object which you want to detect / classify
+7) Inside the "neg" folder copy the images of the random objects which are different from the object which you want to detect / classify
+8) Open the software "Cascade Trainger GUI (version 3.3.1)"
+9) Go to "Train" tab of the software and set the "Positive Image Percentage" = 100 and "Negative Image Count" = Number of Images in the "neg" folder(should be > "pos" folder)
+10) Go to "Common" tab of the software and set the "Number of Stages" = 15, rest everything keep default
+11) Go to "Cascade" tab of the software and set the "Sample Width" = 16, rest everything keep default
+12) Start the training by clicking the "Start" button of the software
+13) Notice in the "images" folder a new subdir will be be created named "classifier" along with 3 other files "neg.lst", "pos.lst" and "pos_samples.vec"
+14) Inside the "classifier" subdir use the file "cascade.xml" for classification.
+
+"""
+
 PATH = "Cascades/haarcascade_watch.xml"
 cameraNo = 0
 objectName = "Watch"
